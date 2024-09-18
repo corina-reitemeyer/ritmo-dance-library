ec83f7c84f550cc9de278ca82a8b035115be6b20
Corina Reitemeyer <corina@corina.org>
2024-09-18 22:18:19 +1200

    Add GET router routes for server

--- server/routes/fruits.ts
+++ server/routes/fruits.ts
@@ -1,17 +1,29 @@
 import { Router } from 'express'
 
-import * as db from '../db/fruits.ts'
+import * as db from '../db/db.ts'
 
 const router = Router()
 
+//GET route 'api/v1/dance-moves'
 router.get('/', async (req, res) => {
   try {
-    const fruits = await db.getAllFruits()
-
-    res.json({ fruits: fruits.map((fruit) => fruit.name) })
+    const moves = await db.getAllMoves()
+    res.json(moves)
   } catch (error) {
     console.log(error)
-    res.status(500).json({ message: 'Something went wrong' })
+    res.status(500).json(`Database error ${error}`)
+  }
+})
+
+//GET route 'api/v1/dance-moves/:id'
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    const danceMoves = await db.getMoveById(id)
+    res.json(danceMoves)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(`Database error ${error}`)
   }
 })
 
