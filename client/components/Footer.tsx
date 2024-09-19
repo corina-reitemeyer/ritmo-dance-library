170460f58df98c0607f50a5a64049ea5c42fc367
Corina Reitemeyer <corina@corina.org>
2024-09-19 20:15:09 +1200

    Add Basic Footer with styling

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <></>
+  return (
+    <footer className="w-full border-t border-gray-200 bg-white py-4">
+      <div className="container mx-auto pt-4 text-center">
+        <p className="mb-4 mt-4">&copy; 2024 Corina Reitemeyer.</p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
