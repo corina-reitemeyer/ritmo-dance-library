720b2656cce671df40e624d3f00f1034f6ab9235
Corina Reitemeyer <corina@corina.org>
2024-09-19 20:20:06 +1200

    Add basic structure for header nav

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom'
+
 function Header() {
-  return <></>
+  return (
+    <nav>
+      <img src="" alt="logo for Ritmo app" />
+      <ul>
+        <li>
+          <Link to="/cuban-salsa">Cuban Salsa</Link>
+          <ul className="dropdown">
+            <li>
+              <Link to="/cuban-salsa/beginners">Beginners</Link>
+            </li>
+            <li>
+              <Link to="/cuban-salsa/intermediate">Intermediate</Link>
+            </li>
+            <li>
+              <Link to="/cuban-salsa/advanced">Advanced</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Header
