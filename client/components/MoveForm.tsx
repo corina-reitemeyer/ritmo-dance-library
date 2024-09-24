b69801861d7871a44dd6636788b9365b45c03505
Corina Reitemeyer <corina@corina.org>
2024-09-24 16:56:14 +1200

    Create form structure for MoveForm

---
+++ client/components/MoveForm.tsx
@@ -0,0 +1,40 @@
+export default function MoveForm() {
+  return (
+    <>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input id="name"></input>
+        <label htmlFor="level">Level</label>
+        <input id="level"></input>
+        <label htmlFor="synonyms">Synonyms</label>
+        <input id="synonyms"></input>
+        <label htmlFor="translation">Translation</label>
+        <input id="translation"></input>
+        <label htmlFor="move_type">Move Type</label>
+        <input id="move_type"></input>
+        <label htmlFor="rueda_type">Rueda Sign</label>
+        <input id="rueda_type"></input>
+        <label htmlFor="starts_with">Starts with</label>
+        <input id="starts_with"></input>
+        <label htmlFor="contains">Contains</label>
+        <input id="contains"></input>
+        <label htmlFor="based_on">Based on</label>
+        <input id="based_on"></input>
+        <label htmlFor="similar_to">Similar to</label>
+        <input id="similar_to"></input>
+        <label htmlFor="bar_counts">Bar counts</label>
+        <input id="bar_counts"></input>
+        <label htmlFor="variations">Variations</label>
+        <input id="variations"></input>
+        <label htmlFor="instructions_for_follower">
+          Instructions for Follower
+        </label>
+        <input id="instructions_for_follower"></input>
+        <label htmlFor="instructions_for_lead">Instructions for Lead</label>
+        <input id="instructions_for_lead"></input>
+        <label htmlFor="source">Video Source</label>
+        <input id="source"></input>
+      </form>
+    </>
+  )
+}
